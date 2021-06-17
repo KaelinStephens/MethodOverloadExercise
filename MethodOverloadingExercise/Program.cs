24defe0c7b0589a6dd3f92db45d614ab4d3f5a04
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-17 12:38:23 -0600

    initial commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool c)
+        {
+            if (c == true)
+            {
+                if (a + b == 1)
+                {
+                    return($"{a + b} dollar");
+                }
+                else
+                {
+                    return ($"{a + b} dollars");
+                }
+            }
+            else
+            {
+                return ($"{a + b}");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
